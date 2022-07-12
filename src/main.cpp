7d059636e4b3d88e8e656e68295a704313ad3233
honey the codewitch <gaztoast@gmail.com>
2022-07-11 23:34:37 -0700

    code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -41,8 +41,11 @@ using color_t = color<typename lcd_t::pixel_type>;
 
 lcd_t lcd;
 
+// declare the MPU that's attached
+// to the first I2C host
 mpu6886 mpu(i2c_container<0>::instance());
-
+// the following is equiv at least on the ESP32
+// mpu6886 mpu(Wire);
 void setup() {
     Serial.begin(115200);
     SPIFFS.begin(false);
